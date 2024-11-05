8b5d8fa54bb4f5527a3a1a13698d73062cc5b8c2
Минтик <121098677+Msakhibullin24@users.noreply.github.com>
2024-11-05 20:09:03 +0300

    Update zadanie-3-b.py

--- zadanie-3-b.py
+++ zadanie-3-b.py
@@ -1,10 +1,19 @@
-'''# Анализ категориальных переменных
+х
+import seaborn as sns
+import pandas as pd
+
+df = sns.load_dataset('mpg')
+
 categorical_cols = df.select_dtypes(include=['object', 'category']).columns
 print(df[categorical_cols].describe().T)  # Показывает count, unique, top, freq для каждой категориальной переменной
 
+
+# Анализ категориальных переменных
+
+
 '''
 
-# или же можно было бы решить так 
+# или же можно было бы решить так  если более подробно и четко расспсиывать 
 import pandas as pd
 
 # Создание DataFrame вручную
@@ -22,3 +31,5 @@ for col in df.select_dtypes(include='object').columns:
     print(f'Количество уникальных значений: {df[col].nunique()}')
     print(f'Мода: {df[col].mode().iloc[0] if not df[col].mode().empty else "Нет моды"}')
     print('-' * 30)
+
+'''
