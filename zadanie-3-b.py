3d0142f28279a81c6bfb18a54c2ccce85eb5010a
Минтик <121098677+Msakhibullin24@users.noreply.github.com>
2024-11-05 20:25:09 +0300

    Update zadanie-3-b.py

--- zadanie-3-b.py
+++ zadanie-3-b.py
@@ -11,7 +11,6 @@ print(df[categorical_cols].describe().T)  # Показывает count, unique, 
 
 
 '''
-
 # или же можно было бы решить так  если более подробно и четко расспсиывать 
 import pandas as pd
 
@@ -30,5 +29,35 @@ for col in df.select_dtypes(include='object').columns:
     print(f'Количество уникальных значений: {df[col].nunique()}')
     print(f'Мода: {df[col].mode().iloc[0] if not df[col].mode().empty else "Нет моды"}')
     print('-' * 30)
+
+'''
+
+#еще можно решить сразу все вместе:
+'''
+import seaborn as sns
+import pandas as pd
+
+df = sns.load_dataset('mpg')
+# Создаем пустой словарь для хранения результатов
+numeric_summary = {}
+
+for col in df.select_dtypes(include=['number']).columns:
+    numeric_summary[col] = {
+        'Доля пропусков': df[col].isnull().mean(),
+        'Максимальное значение': df[col].max(),
+        'Минимальное значение': df[col].min(),
+        'Среднее значение': df[col].mean(),
+        'Медиана': df[col].median(),
+        'Дисперсия': df[col].var(),
+        'Квантиль 0.1': df[col].quantile(0.1),
+        'Квантиль 0.9': df[col].quantile(0.9),
+        'Квартиль 1': df[col].quantile(0.25),
+        'Квартиль 3': df[col].quantile(0.75)
+    }
+
+numeric_summary_df = pd.DataFrame(numeric_summary).T
+
+print("Числовой анализ:")
+print(numeric_summary_df)
 
 '''
